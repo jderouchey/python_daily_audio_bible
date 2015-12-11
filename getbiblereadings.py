e7c614497e28d9c054c47ff36b3068407e69d2d5
jderouchey <joel.derouchey@gmail.com>
2015-12-10 22:41:15 -0800

    Update getbiblereadings.py

--- getbiblereadings.py
+++ getbiblereadings.py
@@ -11,7 +11,7 @@ for x in range (r):
 	y=x+1
 	mylist.insert(x,baseurl + getfile + str(y).zfill(3) + postfix)
 	print mylist[x] # This is for me to see the script in action
-	urllib.urlretrieve(mylist[x],filename = getfile+str(y).zfill(3)+postfix)
+	urllib.urlretrieve(mylist[x], getfile+str(y).zfill(3)+postfix)
 	time.sleep(5)
 # I had to add the sleep timer because the web server would only allow me to download 15 files.
 # I am using python 3.5
